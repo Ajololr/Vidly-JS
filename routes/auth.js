623e7d9bf3b6a02cc3b8ece22814fb0e8a0037d3
Ilya Androsov <ilya_androsov@icloud.com>
2020-10-10 20:59:08 +0300

    feat: add authentication

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const { User } = require("../models/user");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
+
+  res.send(true);
+});
+
+function validate(user) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+}
+
+module.exports = router;
