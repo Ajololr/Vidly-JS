7c50e6b5d19dc89d393c391131608fca89089ace
Ilya Androsov <ilya_androsov@icloud.com>
2020-10-11 15:58:32 +0300

    refactor: make code simpler

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
-require("express-async-errors");
-Joi.objectId = require("joi-objectid")(Joi);
-const winston = require("winston");
-const Joi = require("joi");
-const config = require("config");
 const express = require("express");
-const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
+const winston = require("winston");
+const config = require("config");
 const app = express();
 
-if(!config.get("jwtPrivateKey")) {
-  console.log("Fatal: jwt key is not defined");
-  process.exit(1);
-}
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
-winston.add(winston.transports.File, { filename: "logfile.log" });
+// throw new Error("Unhandled error.")
 
-mongoose
-  .connect("mongodb://localhost/vidly", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to mongo DB..."))
-  .catch(() => console.log("Could not connected to mongo DB..."));
+const p = Promise.reject(new Error("Unhandled promice rejection."))
+p.then(()=> console.log("asd"));
 
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+const PORT = config.get("PORT");
+app.listen(PORT, () => winston.info(`Listening on port ${PORT}...`));
