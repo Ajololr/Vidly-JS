d2657c223f7ddfab0288e4a03977b00ab274dd29
Ilya Androsov <ilya_androsov@icloud.com>
2020-10-30 11:54:14 +0300

    feat: add genres GET /:id endpoint tests

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
 
@@ -24,6 +25,26 @@ describe("/api/genres", () => {
       expect(res.body.length).toBe(2);
       expect(res.body.some((genre) => genre.name === "genre 1")).toBeTruthy();
       expect(res.body.some((genre) => genre.name === "genre 2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return genre with the given id", async () => {
+      const genres = await Genre.collection.insertMany([
+        { name: "genre 1" },
+        { name: "genre 2" },
+      ]);
+
+      const res = await request(server).get(`/api/genres/${genres.ops[0]._id}`);
+      expect(res.status).toBe(200);
+      expect(res.body.name).toBe("genre 1");
+    });
+
+    it("should return error for nonexsiting genre id", async () => {
+      const res = await request(server).get(
+        `/api/genres/${mongoose.Types.ObjectId()}`
+      );
+      expect(res.status).toBe(404);
     });
   });
 });
