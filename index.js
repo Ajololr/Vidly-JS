feb3f56d5ab98381ce733bf665c4fa6d6c561ede
Ilya Androsov <ilya_androsov@icloud.com>
2020-11-07 17:00:53 +0300

    refactor: change port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("./startup/validation")();
 require("./startup/prod")(app);
 
 const PORT = config.get("PORT");
-console.log("PORT:", PORT);
+console.log("PORT:", process.env.PORT);
 const server = app.listen(process.env.PORT || 5555, () =>
   winston.info(`Listening on port ${PORT}...`)
 );
